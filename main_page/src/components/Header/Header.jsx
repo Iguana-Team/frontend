b47cbcb87ecddee33f2bbf9e1a19fbe3afaeb9a1
Oleg Krikunov <okrikunof@gmail.com>
2024-12-01 02:04:36 +0300

    add h1 text

--- main_page/src/components/Header/Header.jsx
+++ main_page/src/components/Header/Header.jsx
@@ -2,9 +2,14 @@ import './Header.css'
 
 function Header() {
     return (
-        <div className="header">
-            <object data="/img/logo/MTS_logo.svg" type="image/svg+xml" alt="MTS logo"/>
-            <img src='/img/portrait/portrait.jpg' alt='profile picture'/>
+        <div>
+            <div className="header">
+                <object data="/img/logo/MTS_logo.svg" type="image/svg+xml" alt="MTS logo"/>
+                <img src='/img/portrait/portrait.jpg' alt='profile picture'/>
+            </div>
+            <div>
+                <h1>Поиск коллеги</h1>
+            </div>
         </div>
     );
   }
