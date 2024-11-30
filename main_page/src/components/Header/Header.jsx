69af620285a050b268be2d3dd6ce6577e18cdef7
Oleg Krikunov <okrikunof@gmail.com>
2024-12-01 01:48:58 +0300

    add profile picture in header

--- main_page/src/components/Header/Header.jsx
+++ main_page/src/components/Header/Header.jsx
@@ -4,6 +4,7 @@ function Header() {
     return (
         <div className="header">
             <object data="/img/logo/MTS_logo.svg" type="image/svg+xml" alt="MTS logo"/>
+            <img src='/img/portrait/portrait.jpg' alt='profile picture'/>
         </div>
     );
   }
