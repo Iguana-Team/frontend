50ce261ced8329f812bbb96be179510bd4d9818e
litvinov <litvinov@kraud.ru>
2024-12-01 04:59:15 +0300

    добавил шифрование данных

--- main_page/src/components/Employee/EmployeeCard.jsx
+++ main_page/src/components/Employee/EmployeeCard.jsx
@@ -4,6 +4,15 @@ import './EmployeeCard.css';
 function EmployeeCard({ employee }) {
     if (!employee) return <div>Loading...</div>;
 
+    const HideValue = (value) => {
+        return value && value.trim() !== "" ? value : "*******";
+    };
+    // шифрование личных данных
+
+    const displayValue = (value) => {
+        return value && value.trim() !== "" ? value : "Данные отсутствуют";
+    };
+
     return (
         <div className="employee-card">
                     <div className="employee-info-columns">
@@ -31,53 +40,53 @@ function EmployeeCard({ employee }) {
                             </div>
 
                             <div className="employee-card-field">
-                                <strong>Телефон:</strong> {employee.phone}
+                                <strong>Телефон:</strong> {HideValue(employee.phone)}
                             </div>
 
                             <div className="employee-card-field">
-                                <strong>Почта:</strong> {employee.email}
-                            </div>
-
-                        </div>
-                        <div className="employee-column">
-
-                            <div className="employee-card-field">
-                                <strong>Должность:</strong> {employee.position}
-                            </div>
-
-                            <div className="employee-card-field">
-                                <strong>Роль:</strong> {employee.role}
-                            </div>
-
-                            <div className="employee-card-field">
-                                <strong>Город:</strong> {employee.city}
-                            </div>
-
-                            <div className="employee-card-field">
-                                <strong>Адрес:</strong> {employee.address}
+                                <strong>Почта:</strong> {HideValue(employee.email)}
                             </div>
 
                         </div>
                         <div className="employee-column">
 
                             <div className="employee-card-field">
-                                <strong>Функциональный блок:</strong> {employee.functionalBlock}
+                                <strong>Должность:</strong> {displayValue(employee.position)}
                             </div>
 
                             <div className="employee-card-field">
-                                <strong>Подразделение 1:</strong> {employee.department1}
+                                <strong>Роль:</strong> {displayValue(employee.role)}
                             </div>
 
                             <div className="employee-card-field">
-                                <strong>Подразделение 2:</strong> {employee.department2}
+                                <strong>Город:</strong> {HideValue(employee.city)}
                             </div>
 
                             <div className="employee-card-field">
-                                <strong>Подразделение 3:</strong> {employee.department3}
+                                <strong>Адрес:</strong> {HideValue(employee.address)}
+                            </div>
+
+                        </div>
+                        <div className="employee-column">
+
+                            <div className="employee-card-field">
+                                <strong>Функциональный блок:</strong> {displayValue(employee.functionalBlock)}
                             </div>
 
                             <div className="employee-card-field">
-                                <strong>Подразделение 4:</strong> {employee.department4}
+                                <strong>Подразделение 1:</strong> {displayValue(employee.department1)}
+                            </div>
+
+                            <div className="employee-card-field">
+                                <strong>Подразделение 2:</strong> {displayValue(employee.department2)}
+                            </div>
+
+                            <div className="employee-card-field">
+                                <strong>Подразделение 3:</strong> {displayValue(employee.department3)}
+                            </div>
+
+                            <div className="employee-card-field">
+                                <strong>Подразделение 4:</strong> {displayValue(employee.department4)}
                             </div>
 
                         </div>
