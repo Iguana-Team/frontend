e21f33579a2ea16a486a660cc4d7a56b477ff9c8
Oleg Krikunov <okrikunof@gmail.com>
2024-12-01 10:23:33 +0300

    chezabrettto

--- main_page/src/App.js
+++ main_page/src/App.js
@@ -8,15 +8,12 @@ import LoginPage from "./components/LoginPage/LoginPage";
 import "./App.css";
 
 function App() {
-  // Состояние для аутентификации
   const [isAuthenticated, setIsAuthenticated] = useState(false);
 
-  // Функция для входа (вызывается после успешного логина)
   const handleLogin = () => {
     setIsAuthenticated(true);
   };
 
-  // Функция для выхода
   const handleLogout = () => {
     setIsAuthenticated(false);
   };
@@ -24,27 +21,25 @@ function App() {
   return (
     <Router>
       <div className="app-wrapper">
-        {isAuthenticated && ( // Показываем Header только для авторизованных пользователей
+        {isAuthenticated && (
           <header>
             <Header />
           </header>
         )}
         <main className="main-content">
           <Routes>
-            {/* Если пользователь не авторизован, перенаправляем на страницу логина */}
             {!isAuthenticated ? (
               <Route path="*" element={<LoginPage onLogin={handleLogin} />} />
             ) : (
               <>
                 <Route path="/" element={<SearchBar />} />
                 <Route path="/employee/:id" element={<EmployeePage />} />
-                {/* 404 Redirect */}
                 <Route path="*" element={<Navigate to="/" />} />
               </>
             )}
           </Routes>
         </main>
-        {isAuthenticated && <Footer />} {/* Показываем Footer только для авторизованных пользователей */}
+        {isAuthenticated && <Footer />}
       </div>
     </Router>
   );
